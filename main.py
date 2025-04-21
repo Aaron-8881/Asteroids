f243bdd8e68378f20e9ba243fd5e638945ffbb66
Aaron-8881 <aaronslifeessentials@gmail.com>
2025-04-21 10:22:36 -0700

    All done!

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collisions(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill((0, 0, 0))
